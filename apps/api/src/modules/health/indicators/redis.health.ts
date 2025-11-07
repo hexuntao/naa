7cb749f3981010fc1e55375c799573545e2e3457
hexuntao <hexuntao@foxmail.com>
2025-11-07 17:47:09 +0800

    v2.2.18

---
+++ apps/api/src/modules/health/indicators/redis.health.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { HealthIndicatorResult, HealthIndicatorService } from '@nestjs/terminus';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisHealthIndicator {
+  constructor(
+    @InjectRedis()
+    private redis: Redis,
+    private healthIndicatorService: HealthIndicatorService,
+  ) {}
+
+  async pingCheck(key: string): Promise<HealthIndicatorResult> {
+    const indicator = this.healthIndicatorService.check(key);
+    try {
+      const result = await this.redis.ping();
+      if (result !== 'PONG') {
+        throw new Error('Invalid PING response');
+      }
+      return indicator.up();
+    } catch (error: any) {
+      return indicator.down(error.message);
+    }
+  }
+}
